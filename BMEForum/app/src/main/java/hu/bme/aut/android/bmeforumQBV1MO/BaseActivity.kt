eed901082ba51fed372440e6a79f2bb54aac95d4
Dorothy2019 <47299442+Dorothy2019@users.noreply.github.com>
2020-04-12 20:53:12 +0200

    .

---
+++ BMEForum/app/src/main/java/hu/bme/aut/android/bmeforumQBV1MO/BaseActivity.kt
@@ -0,0 +1,44 @@
+package hu.bme.aut.android.bmeforumQBV1MO
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    private var progressDialog: ProgressDialog? = null
+
+    private val firebaseUser: FirebaseUser?
+        get() = FirebaseAuth.getInstance().currentUser
+
+    protected val uid: String?
+        get() = firebaseUser?.uid
+
+    protected val userName: String?
+        get() = firebaseUser?.displayName
+
+    protected val userEmail: String?
+        get() = firebaseUser?.email
+
+    fun showProgressDialog() {
+        if (progressDialog != null) {
+            return
+        }
+
+        progressDialog = ProgressDialog(this).apply {
+            setCancelable(false)
+            setMessage("Loading...")
+            show()
+        }
+    }
+
+    protected fun hideProgressDialog() {
+        progressDialog?.let { dialog ->
+            if (dialog.isShowing) {
+                dialog.dismiss()
+            }
+        }
+        progressDialog = null
+    }
+
+    protected fun toast(message: String?) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
